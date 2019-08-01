b1c58939a5c282263b5a48b90b8cc4db87f633a1
SulaymanSaleem <39617382+SulaymanSaleem@users.noreply.github.com>
2019-08-01 09:56:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir opt
 cd opt
 mkdir install_dir
 cd install_dir
-#wget "https://sonatype-download.global.ssl.fastly.net/repository/repositoryManager/3/nexus-3.18.0-01-unix.tar.gz"
-#tar xvzf nexus-3.18.0-01-unix.tar.gz
+wget "https://sonatype-download.global.ssl.fastly.net/repository/repositoryManager/3/nexus-3.18.0-01-unix.tar.gz"
+tar xvzf nexus-3.18.0-01-unix.tar.gz
 cd nexus-3.18.0-01/bin
 ./nexus run
