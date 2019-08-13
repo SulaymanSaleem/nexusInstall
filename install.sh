92f729b7aa3b9a3ff9b24ced88c0e2cb10b4943a
SulaymanSaleem <39617382+SulaymanSaleem@users.noreply.github.com>
2019-08-13 16:17:23 +0100

    Update install.sh
    changing mkdir and cd

--- install.sh
+++ install.sh
@@ -3,10 +3,8 @@
 #install java and jvm
 
 sudo apt install -y openjdk-8-jdk
-mkdir opt
-cd opt
-mkdir install_dir
-cd install_dir
+mkdir -p opt/install_dir
+cd opt/install_dir
 wget "https://sonatype-download.global.ssl.fastly.net/repository/repositoryManager/3/nexus-3.18.0-01-unix.tar.gz"
 tar xvzf nexus-3.18.0-01-unix.tar.gz
 cd nexus-3.18.0-01/bin
